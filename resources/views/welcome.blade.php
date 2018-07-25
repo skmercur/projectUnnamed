90b0114e846da80185c94a2e20b5ba21aa11ffd2
HichemMerniz <38223636+HichemMerniz@users.noreply.github.com>
2018-07-25 22:13:06 +0100

    remplace

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -43,11 +43,16 @@
                                 <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                             </li>
                         @else
+                  
                             <li class="nav-item dropdown">
+                            
+                            @foreach($users to $user)
+                            <img src="{{ 'uploads/'.user()->imagpath}}" alt="">
+                            @endforeach
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                {{ Auth::user()->name}} <span class="caret"></span>
                                 </a>
-
+                               
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
@@ -60,6 +65,7 @@
                                     </form>
                                 </div>
                             </li>
+                           
                         @endguest
                     </ul>
 
@@ -69,6 +75,7 @@
        
       
     </nav>
+
     <!-- /////////////////////////////////////// -->
     <br>
     <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
