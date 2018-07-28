3479183500c58fa85c66ef98caa149cdc203982f
skmercur <khoudoursofiane75@gmail.com>
2018-07-28 14:56:30 +0100

    new stuff

--- resources/views/userprofile.blade.php
+++ resources/views/userprofile.blade.php
@@ -3,10 +3,17 @@
 @guest
 <h3>error , it seeams that you are not connected  </h3>
 @else
-@if($user->username != Auth::user()->username)
-<h3> Welcome to , {{$user->name}} profile</h3>
+@if(empty($user->username))
+<h3>error , user doesn't exist </h3>
 @else
-<h3> Welcome, {{Auth::user()->name}}</h3>
+@if($user->username != Auth::user()->username)
+<h3> Welcome to  {{$user->lastname}} {{$user->firstname}} profile</h3>
+<img src="{{$user->imgpath}}" width="120" height="120" />
+
+@else
+<h3> Welcome, {{Auth::user()->lastname}} {{Auth::user()->firstname}}</h3>
+<img src="{{Auth::user()->imgpath}}" width="120" height="120" />
+@endif
 @endif
 @endguest
 @endsection
