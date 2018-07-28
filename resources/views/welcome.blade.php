c3fae77fa1f78b160ae05267d720d0a07467b5f2
HichemMerniz <38223636+HichemMerniz@users.noreply.github.com>
2018-07-28 14:13:12 +0100

    recherche input

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,6 +12,7 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
+   
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
@@ -25,7 +26,7 @@
 <body>
 <div id="app">
 
-<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+<nav class="navbar navbar-expand-md navbar-light fixed-top bg-light">
 <div class="container">
       <a class="navbar-brand" href="#">TeamDz</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
@@ -94,142 +95,27 @@
 
 
     </nav>
+    </div>
+    <hr>
+    <hr>
+    <form action=""style="margin-top: 300px;">
 
-    <!-- /////////////////////////////////////// -->
-    <br>
-    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-  <ol class="carousel-indicators">
-    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-  </ol>
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img class="d-block w-100" src="{{asset('assets/img/pexels-photo-1068523.jpg')}}" alt="First slide"  width="800" height="600">
-      <div class="container">
-              <div class="carousel-caption text-left">
-                <h1>Actu 1</h1>
-                <p>Resumé de contenu ......</p>
-                <p><a class="btn btn-lg btn-primary" href="#" role="button">Reed More</a></p>
-              </div>
-            </div>
-
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="{{asset('assets/img/pexels-photo-169573.jpg')}}" alt="Second slide"  width="800" height="600" >
-      <div class="carousel-caption text-right">
-                <h1>Actu 2</h1>
-                <p>Resumé de contenu ......</p>
-                <p><a class="btn btn-lg btn-primary" href="#" role="button">Reed More</a></p>
-              </div>
-            </div>
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="{{asset('assets/img/pexels-photo-1068523.jpg')}}" alt="Third slide">
-      <div class="container">
-              <div class="carousel-caption text-left">
-                <h1>Actu 3</h1>
-                <p>Resumé de contenu ......</p>
-                <p><a class="btn btn-lg btn-primary" href="#" role="button">Reed More</a></p>
-              </div>
-            </div>
-    </div>
-  </div>
-  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
+    <div class="input-group mb-12 center">
+  <input type="text" class="form-control form-control-lg" style="margin-left: 15%;margin-right: 15%;" placeholder="Recherche">
+  
+  
+  
+  
 </div>
 <br>
-
-<!--////////////////////////////////////////////////////////-->
-
-<!-- Marketing messaging and featurettes
-================================================== -->
-<!-- Wrap the rest of the page in another container to center all the content. -->
-
-<div class="container marketing">
-
-  <!-- Three columns of text below the carousel -->
-  <div class="row">
-    <div class="col-lg-4">
-      <img class="rounded-circle" src="{{ asset('assets/img/Hichem.jpg') }}" alt="Generic placeholder image" width="140" height="140">
-      <h2>Hichem Merniz</h2>
-      <p>.</p>
-      <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
-    </div><!-- /.col-lg-4 -->
-    <div class="col-lg-4">
-      <img class="rounded-circle" src="{{ asset('assets/img/Sofiane.jpg') }}" width="140" height="140">
-      <h2>Khoudour Sofiane</h2>
-      <p>.</p>
-      <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
-    </div><!-- /.col-lg-4 -->
-    <div class="col-lg-4">
-      <img class="rounded-circle" src="{{ asset('assets/img/Walid.jpg') }}" alt="Generic placeholder image" width="140" height="140">
-      <h2>Msili Walid</h2>
-      <p>.</p>
-      <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
-    </div><!-- /.col-lg-4 -->
-  </div><!-- /.row -->
-
-
-  <!-- START THE FEATURETTES -->
-
-  <hr class="featurette-divider">
-
-  <div class="row featurette">
-    <div class="col-md-7">
-      <h2 class="featurette-heading">First featurette heading. <span class="text-muted">It'll blow your mind.</span></h2>
-      <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
-    </div>
-    <div class="col-md-5">
-      <img class="featurette-image img-fluid mx-auto"  alt="500x500" style="width: 500px; height: 500px;" src="{{ asset('assets/img/pexels-photo-356056.jpg') }}" data-holder-rendered="true">
-    </div>
+  <br>
+<div class="input-group" >
+    <button class="form-control btn btn-success" style="margin-left: 30%;margin-right: 30%;" type="button" id="button-addon2">Recherche</button>
   </div>
 
-  <hr class="featurette-divider">
 
-  <div class="row featurette">
-    <div class="col-md-7 order-md-2">
-      <h2 class="featurette-heading">Oh yeah, it's that good. <span class="text-muted">See for yourself.</span></h2>
-      <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
-    </div>
-    <div class="col-md-5 order-md-1">
-      <img class="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="500x500" style="width: 500px; height: 500px;" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22500%22%20height%3D%22500%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20500%20500%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_164c7ae3cf9%20text%20%7B%20fill%3A%23AAAAAA%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A25pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_164c7ae3cf9%22%3E%3Crect%20width%3D%22500%22%20height%3D%22500%22%20fill%3D%22%23EEEEEE%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22183.5%22%20y%3D%22259.3%22%3E500x500%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" data-holder-rendered="true">
-    </div>
-  </div>
+  
+</form>
 
-  <hr class="featurette-divider">
-
-  <div class="row featurette">
-    <div class="col-md-7">
-      <h2 class="featurette-heading">And lastly, this one. <span class="text-muted">Checkmate.</span></h2>
-      <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
-    </div>
-    <div class="col-md-5">
-      <img class="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="500x500" style="width: 500px; height: 500px;" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22500%22%20height%3D%22500%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20500%20500%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_164c7ae3d01%20text%20%7B%20fill%3A%23AAAAAA%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A25pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_164c7ae3d01%22%3E%3Crect%20width%3D%22500%22%20height%3D%22500%22%20fill%3D%22%23EEEEEE%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22183.5%22%20y%3D%22259.3%22%3E500x500%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" data-holder-rendered="true">
-    </div>
-  </div>
-
-  <hr class="featurette-divider">
-
-  <!-- /END THE FEATURETTES -->
-
-</div><!-- /.container -->
-
-
-<!-- FOOTER -->
-<footer class="container">
-  <p class="float-right"><a href="#">Back to top</a></p>
-  <p>© 2017-2018 Company, Inc. · <a href="#">Privacy</a> · <a href="#">Terms</a></p>
-</footer>
-</main>
-
-    <link rel="stylesheet" href="{{asset('assets/js/bootstrap.min.js')}}">
-    <link rel="stylesheet" href="{{asset('assets/js/jquery.js')}}">
     </body>
 </html>
