2873d16eb6ad5bbbb8d0ac33bcdcd77959d51105
HichemMerniz <38223636+HichemMerniz@users.noreply.github.com>
2018-07-28 15:51:00 +0100

    profil fixed

--- resources/views/userprofile.blade.php
+++ resources/views/userprofile.blade.php
@@ -3,12 +3,8 @@
 @guest
 <h3>error , it seeams that you are not connected  </h3>
 @else
-@if(empty($user->username))
-<h3>error , user doesn't exist </h3>
-@else
 @if($user->username != Auth::user()->username)
-<<<<<<< HEAD
-<h3> Welcome to , {{$user->name}} profile</h3>
+
 <br>
 <div class="container">
   <div class="row">
@@ -21,20 +17,20 @@
           <div class="row">
 			<div class="col-md-4 text-center">
               <img class="img-circle avatar avatar-original" style="-webkit-user-select:none; 
-              display:block; height: 140px; margin:auto;" src="{{ Auth::user()->imgpath}}" style="">
+              display:block; height: 140px; margin:auto;" src="{{ $user->imgpath}}" style="">
             </div>
             <div class="col-md-8">
               <div class="row">
                 <div class="col-md-12">
-                  <h1 class="only-bottom-margin">{{Auth::user()->username}}</h1>
+                  <h1 class="only-bottom-margin">{{$user->firstname}}{{$user->lastname}}</h1>
                 </div>
               </div>
               <div class="row">
                 <div class="col-md-6">
-                  <span class="text-muted">Email:</span> {{Auth::user()->email}}<br>
+                  <span class="text-muted">Email:</span> {{$user->email}}<br>
                   <span class="text-muted">Birth date:</span> 01.01.2001<br>
                   <span class="text-muted">Gender:</span> male<br><br>
-                  <small class="text-muted">Created: 01.01.2015</small>
+                  <small class="text-muted">Created: {{$user->created_at}} </small>
                 </div>
                 <div class="col-md-6">
                   <div class="activity-mini">
@@ -72,7 +68,7 @@
             <div class="col-md-8">
               <div class="row">
                 <div class="col-md-12">
-                  <h1 class="only-bottom-margin">{{Auth::user()->firstname}}{{Auth::user()->lastname}}</h1>
+                  <h1 class="only-bottom-margin">{{Auth::user()->firstname}} {{Auth::user()->lastname}}</h1>
                 </div>
               </div>
               <div class="row">
@@ -95,7 +91,9 @@
           </div>
           <div class="row">
             <div class="col-md-12">
-              <hr><button class="btn btn-default pull-right"><i class="glyphicon glyphicon-pencil"></i> Edit</button>
+              <hr>
+              <hr>
+              <button class="btn btn-outline-primary"><i class="glyphicon glyphicon-pencil"></i> Edit</button>
             </div>
           </div>
         </div>
@@ -103,15 +101,6 @@
     </div>
   </div>
 </div>
-=======
-<h3> Welcome to  {{$user->lastname}} {{$user->firstname}} profile</h3>
-<img src="{{$user->imgpath}}" width="120" height="120" />
-
-@else
-<h3> Welcome, {{Auth::user()->lastname}} {{Auth::user()->firstname}}</h3>
-<img src="{{Auth::user()->imgpath}}" width="120" height="120" />
-@endif
->>>>>>> 7cd6406d8daf2b7f54e76725f3af51d20f4b3e5a
 @endif
 @endguest
 @endsection
