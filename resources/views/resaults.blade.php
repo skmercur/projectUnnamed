f0bac5bd75963ac8069efddb16bafac6bbc4c974
skmercur <khoudoursofiane75@gmail.com>
2018-07-29 20:44:47 +0100

    fixed errors

--- resources/views/resaults.blade.php
+++ resources/views/resaults.blade.php
@@ -1,24 +1,27 @@
 @extends('layouts.layout')
-@foreach($resaults as $resault)
+
 
 
 
 <div class="container">
 	<div class="col-md-9 col-md-pull-3">
-        <h1 class="search-results-count" style="margin-top: 7%;margin-left: 50%;">Searsh Results</h1>
+        <h1 class="search-results-count" style="margin-top: 7%;margin-left: 50%;">Search Results</h1>
+
         <section class="search-result-item">
-            @foreach($resaults as $resault)
+@foreach($resaults as $resault)
             <div class="search-result-item-body">
                 <div class="row">
                     <div class="col-sm-9">
                         <h4 class="search-result-item-heading"><a href="#">{{$resault->title}}</a></h4>
 
-                        <p class="description"><?php if(strlen($file->description)>200) echo substr($file->description,0,200)."..."; ?></p>
+                        <p class="description"><?php if(strlen($resault->description)>200) echo substr($resault->description,0,200)."...";else {
+                        	echo $resault->description;
+                        } ?></p>
                     </div>
                 </div>
             </div>
-            @endforeach
+  @endforeach
         </section>
+
     </div>
 </div>
-@endforeach
