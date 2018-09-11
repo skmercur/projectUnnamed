ae7efe05b9d8eae67179ed1bafd77243c5a7165e
VirWus <walidvirwus@gmail.com>
2018-09-11 21:01:32 +0100

    erzarxcfxfds

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -172,110 +172,110 @@
     @guest
 
 
-<div class="timeline-cover">
+
+     
+<div class="timeline-cover" >
 
           <!--Timeline Menu for Large Screens-->
           <div class="timeline-nav-bar hidden-sm hidden-xs">
-            <div class="row">
-              <div class="col-md-3">
-                <div class="profile-info">
-                  <img src="http://placehold.it/300x300" alt="" class="img-responsive profile-photo" />
-                  <h3>Sarah Cruiz</h3>
-                  <p class="text-muted">Creative Director</p>
-                </div>
-              </div>
-              <div class="col-md-9">
-                <ul class="list-inline profile-menu">
-                  <li><a href="timeline.html" class="active">Timeline</a></li>
-                  <li><a href="timeline-about.html">About</a></li>
-                  <li><a href="timeline-album.html">Album</a></li>
-                  <li><a href="timeline-friends.html">Friends</a></li>
-                </ul>
-                <ul class="follow-me list-inline">
-                  <li>1,299 people following her</li>
-                  <li><button class="btn-primary">Add Friend</button></li>
-                </ul>
-              </div>
-            </div>
-          </div><!--Timeline Menu for Large Screens End-->
-
-          <!--Timeline Menu for Small Screens-->
-          <div class="navbar-mobile hidden-lg hidden-md">
-            <div class="profile-info">
-              <img src="http://placehold.it/300x300" alt="" class="img-responsive profile-photo" />
-              <h4>Sarah Cruiz</h4>
-              <p class="text-muted">Creative Director</p>
-            </div>
-            <div class="mobile-menu">
-              <ul class="list-inline">
-                <li><a href="timline.html" class="active">Timeline</a></li>
-                <li><a href="timeline-about.html">About</a></li>
-                <li><a href="timeline-album.html">Album</a></li>
-                <li><a href="timeline-friends.html">Friends</a></li>
-              </ul>
-              <button class="btn-primary">Add Friend</button>
-            </div>
-          </div><!--Timeline Menu for Small Screens End-->
-
-        </div>
-
-
-    <form action="search" style="margin-top: 15px;" method="get">
-     <div class="container">
-        <div class="row">
-        <div class="col-sm-12">
-                <img src="{{asset('assets/img/logo1.png')}}" class="img-fluid" style="margin-left: auto;
+          
+   <div class="col-sm-12 fadeIn" >
+                <img src="{{asset('assets/img/logo1white.png')}}" class="img-fluid animated fadeIn" style="margin-left: auto;
   margin-right: auto;
-  display: block;max-height: 260px;">
+  display: block;max-height: 370px;">
         </div>
-      <div class="input-group mb-12 center">
+              <div class="col-md-12">
+                <div class="profile-info">
+                <button onclick="openSearch()" class="btn btn-default btn-circle btn-xl animated pulse infinite"><i class="fa fa-search"></i></button>
 
-          <div class="col-sm-12 col-xs-6">
-                    <input name="q" type="text" id="navbarsearch1" onkeydown="readThat1()" placeholder="Search for documents" autocomplete="off" >
-                    <input id="search_submit" value="Rechercher" type="submit">
-         </div>
-         <div class="col-sm-12" style="margin-top: 12vh;background-color:#FFF;">
-                  <div id="countryList1" style="width:80%;float:left"></div>
+                </div>
+                 
+              </div>
+               
+             
+       
 
             </div>
+</div><!--Timeline Menu for Large Screens End-->
 
+ 
+     <!--Timeline Menu for Small Screens End-->
+
+
+<div id="myOverlay" class="animated zoomIn faster overlay">
+  <span class="closebtn" onclick="closeSearch()" title="Close Overlay">×</span>
+
+  <div class="container">
+  <div class="row">
+    <div class=" col-sm-12">
+       <form action="usearch" method="get">
+        
+    <input name="q" type="search" id="navbarsearch" onkeydown="readThat()" placeholder="Search for people and documents" autocomplete="off" list="navbarsearchDataList" id="navbarsearch" >
 
       </div>
-       </div>
-  </div>
-  </form>
 
-  <section style="margin-top: 4%;margin-bottom: 10%;" class="gray-bg section-padding" id="feature-page">
+                <div class="col-sm-12 pre-scrollable" style="">
+                       <div id="countryList"  style="">
+                         
+                       </div>
+
+                  </div>
+
+      <div class="col-sm-12">
+    </div>
+
+    </div>
+        </form>
+  </div>
+</div>
+
+</div>
+
+</div>
+
+
+
+ <section style="margin-top: 10%;margin-bottom: 10%;" class="gray-bg section-padding" id="feature-page">
         <div class="container">
 
             <div class="row">
 
-                          @foreach($spec as $s)
+            @foreach($spec as $s)
 
+                <div class="animated zoomIn col-xs-12 col-sm-6 col-md-4">
+                    <a href="/search?new=<?php echo base64_encode(encrypt($s->namespi)); ?>">
+                    <div class="box">
+                          <div class="box-icon">
+                        <img src="assets/img/{{$s->imspi}}" alt="">
+                          </div>
+                        <h3>{{$s->namespi}}</h3>
+                        <ul  class="list-group" >
+                          <?php $i=1; ?>
+                        @foreach($s->files as $ss)
 
+                        <li class="list-group-item"><?php echo $i."- "; ?>{{$ss->title}}</li>
+                        <?php $i++; ?>
+                        @endforeach
+                        </ul>
+                    </div>
 
-                              <div class="col-xs-12 col-sm-6 col-md-4">
-                                  <a href="/search?new=<?php echo base64_encode(encrypt($s->namespi)); ?>">
-                                  <div class="box">
-                                        <div class="box-icon">
-                                      <img src="assets/img/{{$s->imspi}}" alt="">
-                                        </div>
-                                      <h3>{{$s->namespi}}</h3>
-                                      <ul  class="list-group" >
-                                        <?php $i=1; ?>
-                                      @foreach($s->files as $ss)
-                                      <li class="list-group-item"><?php echo $i."- "; ?>{{$ss->title}}</li>
-                                      <?php $i++; ?>
-                                      @endforeach
-                                      </ul>
-                                  </div>
-
-                                </a>
-                              </div>
-                          @endforeach
+                  </a>
+                </div>
+            @endforeach
             </div>
         </div>
     </section>
+ 
+<script>
+
+function openSearch() {
+    document.getElementById("myOverlay").style.display = "block";
+}
+
+function closeSearch() {
+    document.getElementById("myOverlay").style.display = "none";
+}
+</script>
 
 @else
 
