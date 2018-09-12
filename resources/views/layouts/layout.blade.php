a5b18fc490cefd70629b770ac41c0c817909fc2c
khoudour sofiane <khoudoursofiane75@gmail.com>
2018-09-12 09:28:52 +0100

    uu

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -16,7 +16,6 @@
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" type="text/javascript"> </script>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" type="text/javascript"></script>
-
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" type="text/javascript"></script>
 <link href="https://gitcdn.github.io/bootstrap-toggle/2.2.2/css/bootstrap-toggle.min.css" rel="stylesheet">
 <script src="https://gitcdn.github.io/bootstrap-toggle/2.2.2/js/bootstrap-toggle.min.js"></script>
@@ -26,6 +25,8 @@
 <script src="{{ asset('assets/js/iziToast.min.js') }}" type="text/javascript"></script>
 
   <script src="{{asset('assets/js/headroom.min.js')}}"></script>
+
+
 
  <script src="{{asset('assets/js/argon.js')}}"></script>
 
@@ -63,7 +64,7 @@
   <link href="{{ asset('css/all.css') }}" rel="stylesheet">
 
   <link href="{{ asset('assets/DataTables/datatables.min.css') }}" rel="stylesheet">
- 
+
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.4.1/cropper.min.css" />
 
@@ -387,7 +388,7 @@ echo base64_encode(encrypt(Auth::user()->username));
 
 
 </body>
-
+<script src="https://cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.2/rollups/aes.js"></script>
 <script>
     $("#menu-toggle").click(function(e) {
         e.preventDefault();
